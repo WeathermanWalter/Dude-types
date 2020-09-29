0197356b25c180ddad5aa2f8002bbbb35fac1ada
David Wordelman <DavidWordelman@gmail.com>
2020-09-28 17:24:54 -0700

    initial commit

---
+++ examples/typeusage.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "types.h"
+
+int_type_dude x = 100;
+float_type_dude f = 1.0;
+unsigned_type long_type_dude l = 0;
+
+int_type_dude main() {
+	char_type_dude buffer[100];
+	printf("Start typing, my dude type dude\n");
+	scanf("%s", buffer);
+	printf("You entered \n%s\nmy dude\n", buffer);
+}
